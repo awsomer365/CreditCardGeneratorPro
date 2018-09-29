60b09ea1da00e4bd9ed8486f6f47855b3a55b4f1
ian <awsomer365@gmail.com>
2018-09-28 19:53:53 -0700

    Integrated Fragments with working buttons. Added ads. Needs UI

---
+++ app/src/main/java/com/blogspot/httpgplaysupport/creditcardgeneratorpro/CheckFragment.java
@@ -0,0 +1,386 @@
+package com.blogspot.httpgplaysupport.creditcardgeneratorpro;
+
+
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+
+import org.w3c.dom.Text;
+
+import static com.blogspot.httpgplaysupport.creditcardgeneratorpro.MainFragment.calculateCheckDigit;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CheckFragment extends Fragment implements View.OnClickListener, RewardedVideoAdListener {
+
+
+    public CheckFragment() {
+        // Required empty public constructor
+    }
+
+    private CheckBox modCheckBx, IINCheckBx, UNCheckBx;
+
+    private RewardedVideoAd mRewardedVideoAd;
+
+    private EditText cardInput;
+    private TextView iinText;
+    private TextView unText;
+    private TextView luhnText;
+    private TextView checkText;
+
+    private Button checkButton;
+    private Button switchFragButton;
+    private Button unlockButton;
+
+
+    private String cardIssuer;
+    private int cardUnStringVal;
+
+    private int checkInt;
+    private int rewardState = 0;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myFragmentView = inflater.inflate(R.layout.fragment_check, container, false);
+
+        modCheckBx = myFragmentView.findViewById(R.id.modCheckBx);
+        IINCheckBx = myFragmentView.findViewById(R.id.IINCheckBx);
+
+        cardInput = myFragmentView.findViewById(R.id.cardInput);
+        cardInput.setText(MainFragment.finalCardNumString);
+        iinText = myFragmentView.findViewById(R.id.iinText);
+        unText = myFragmentView.findViewById(R.id.unText);
+        luhnText = myFragmentView.findViewById(R.id.luhnText);
+        checkText = myFragmentView.findViewById(R.id.checkRemainTxt);
+
+        modCheckBx.setEnabled(false);
+        IINCheckBx.setEnabled(false);
+
+        modCheckBx.setChecked(false);
+        IINCheckBx.setChecked(false);
+
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(getActivity());
+        mRewardedVideoAd.setRewardedVideoAdListener(this);
+        loadRewardedVideoAd();
+
+        checkButton = myFragmentView.findViewById(R.id.checkButton);
+        checkButton.setOnClickListener(this);
+
+        switchFragButton = myFragmentView.findViewById(R.id.switchGenButton);
+        switchFragButton.setOnClickListener(this);
+
+        unlockButton = myFragmentView.findViewById(R.id.unlockBtn);
+        unlockButton.setOnClickListener(this);
+
+        getUnlockKeyValue();
+
+        checkText.setText("Checks: " + checkInt);
+
+        if (checkInt <= 0){
+            checkInt = 0;
+        }
+
+        return myFragmentView;
+    }
+
+    private void getUnlockKeyValue() {
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("unlocks_key", Context.MODE_PRIVATE);
+
+        int defaultValue = getResources().getInteger(R.integer.unlockIntValue);
+        checkInt = sharedPreferences.getInt(getString(R.string.unlocks_key), defaultValue);
+
+        if (checkInt <= 0) {
+            checkInt = 0;
+        }
+    }
+
+    private void setUnlockKeyValue() {
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("unlocks_key", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(getString(R.string.unlocks_key), checkInt);
+
+        editor.commit();
+    }
+
+    @Override
+    public void onClick(View v){
+
+        switch (v.getId()) {
+            case R.id.unlockBtn:
+
+                if (mRewardedVideoAd.isLoaded()) {
+                    mRewardedVideoAd.show();
+                    rewardState = 0;
+                }
+                else if (rewardState == 3){
+                    Toast.makeText(getActivity(),
+                            "No Ads At This Time, Try Again Later",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getActivity(),
+                            "Failed To Load Ad, Try Again Later. Error Code " + rewardState,
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+
+        }
+
+        switch (v.getId()) {
+            case R.id.switchGenButton:
+                Log.d("Credit Card Number: ", "Value: ");
+                final FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.mainLayout, new MainFragment());
+                ft.commit();
+                break;
+
+        }
+        switch (v.getId()){
+            case R.id.checkButton:
+
+                // do something when the button is clicked
+
+                String inputNum = cardInput.getText().toString();
+
+                //Checks if user input is exactly 16 digits. No more or less
+                if (cardInput.getText().toString().trim().length() < 16 || cardInput.getText().toString().trim().length() > 16) {
+                    Toast.makeText(getActivity(), "Please Input a 16 Digit Card Number", Toast.LENGTH_SHORT).show();
+                    invalidError();
+                } else {
+                    if (checkInt > 0) {
+                        if (luhnCheck(inputNum)) {
+                            modCheckBx.setChecked(true);
+                            luhnText.setTextColor((this.getResources().getColor(R.color.text_green)));
+                            luhnText.setText(inputNum.substring(15));
+                        } else {
+                            modCheckBx.setChecked(false);
+                            luhnText.setTextColor((this.getResources().getColor(R.color.text_red)));
+                            luhnText.setText(R.string.invalid_code);
+                        }
+
+                        checkIIN(inputNum);
+                        checkUn(inputNum);
+
+                        checkInt -= 1;
+                        checkText.setText("Checks: " + checkInt);
+                    } else{
+                        checkInt = 0;
+                        invalidError();
+                        Toast.makeText(getActivity(),
+                                "No Checks Remaining",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+                iinText.setText(cardIssuer);
+
+        }
+    }
+
+    public void invalidError(){
+        modCheckBx.setChecked(false);
+        luhnText.setText(R.string.invalid_code);
+        luhnText.setTextColor((this.getResources().getColor(R.color.text_red)));
+        IINCheckBx.setChecked(false);
+        iinText.setTextColor(this.getResources().getColor(R.color.text_red));
+        cardIssuer = "Invalid Number";
+        unText.setTextColor((this.getResources().getColor(R.color.text_red)));
+        unText.setText(R.string.invalid_code);
+    }
+
+    private void loadRewardedVideoAd() {
+        mRewardedVideoAd.loadAd("ca-app-pub-9282720184335688/6522830889",
+                new AdRequest.Builder().build());
+    }
+
+    //LuhnCheck algorithm to see if last digit correlates with rest of card number
+    public static boolean luhnCheck(String card) {
+        if (card == null)
+            return false;
+        char checkDigit = card.charAt(card.length() - 1);
+        String digit = calculateCheckDigit(card.substring(0, card.length() - 1));
+        return checkDigit == digit.charAt(0);
+    }
+
+    //CheckIIn identifies if user input number has a recognized IIN
+    public void checkIIN(String card) {
+        int i;
+        int x;
+        boolean foundIIN = false;
+        Resources res = getResources();
+        String[] issuerArray = res.getStringArray(R.array.card_arrays);
+        x = 4;
+        if (card.indexOf(String.valueOf(x)) == 0) {
+            cardIssuer = issuerArray[0];
+            foundIIN = true;
+            cardUnStringVal = String.valueOf(x).length();
+        }
+        for (x = 51; x <= 55; x++) {
+            if (card.indexOf(String.valueOf(x)) == 0) {
+                cardIssuer = issuerArray[1];
+                foundIIN = true;
+                cardUnStringVal = String.valueOf(x).length();
+            }
+        }
+        x = 34;
+        for (i = 0; i <= 1; i++) {
+            if (card.indexOf(String.valueOf(x)) == 0) {
+                cardIssuer = issuerArray[2];
+                foundIIN = true;
+                cardUnStringVal = String.valueOf(x).length();
+            }
+            x = 37;
+        }
+        x = 6011;
+        for (i = 0; i <= 2; i++) {
+            if (i == 2) {
+                x = 65;
+            }
+            if (card.indexOf(String.valueOf(x)) == 0) {
+                cardIssuer = issuerArray[3];
+                foundIIN = true;
+                cardUnStringVal = String.valueOf(x).length();
+            }
+            x = 64;
+        }
+        x = 3095;
+        for (i = 0; i <= 2; i++) {
+            if (card.indexOf(String.valueOf(x)) == 0) {
+                cardIssuer = issuerArray[4];
+                foundIIN = true;
+                cardUnStringVal = String.valueOf(x).length();
+            } else if (i == 1) {
+                for (x = 300; x <= 305; x++) {
+                    if (card.indexOf(String.valueOf(x)) == 0) {
+                        cardIssuer = issuerArray[4];
+                        foundIIN = true;
+                        cardUnStringVal = String.valueOf(x).length();
+                    }
+                }
+            } else if (i == 2) {
+                for (x = 38; x <= 39; x++) {
+                    if (card.indexOf(String.valueOf(x)) == 0) {
+                        cardIssuer = issuerArray[4];
+                        foundIIN = true;
+                        cardUnStringVal = String.valueOf(x).length();
+                    }
+                }
+            }
+        }
+        x = 62;
+        if (card.indexOf(String.valueOf(x)) == 0) {
+            cardIssuer = issuerArray[5];
+            foundIIN = true;
+            cardUnStringVal = String.valueOf(x).length();
+        }
+        x = 60;
+        for (i = 0; i <= 1; i++) {
+            if (card.indexOf(String.valueOf(x)) == 0 && card.indexOf(String.valueOf(x + 5951)) != 0) {
+                cardIssuer = issuerArray[6];
+                foundIIN = true;
+                cardUnStringVal = String.valueOf(x).length();
+            }
+            x = 6521;
+        }
+        for (x = 3528; x <= 3589; x++) {
+            if (card.indexOf(String.valueOf(x)) == 0) {
+                cardIssuer = issuerArray[7];
+                foundIIN = true;
+                cardUnStringVal = String.valueOf(x).length();
+            }
+        }
+        if (!foundIIN) {
+            cardUnStringVal = 0;
+            cardIssuer = "Not Recognized";
+            iinText.setTextColor(this.getResources().getColor(R.color.text_red));
+            IINCheckBx.setChecked(false);
+        } else {
+            iinText.setTextColor(this.getResources().getColor(R.color.text_green));
+            IINCheckBx.setChecked(true);
+        }
+    }
+
+    public void checkUn(String card) {
+        unText.setTextColor(this.getResources().getColor(R.color.text_green));
+        String personalAcctNum = card.substring(cardUnStringVal, card.length() - 1);
+        unText.setText(personalAcctNum);
+    }
+
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+
+        loadRewardedVideoAd();
+
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+        checkInt = checkInt + rewardItem.getAmount();
+        setUnlockKeyValue();
+        checkText.setText("Checks: " + checkInt);
+
+
+        Toast.makeText(getActivity(),
+                "Checks Successfully Added",
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+
+        rewardState = i;
+
+    }
+
+    @Override
+    public void onRewardedVideoCompleted() {
+
+    }
+}
---
+++ app/src/main/java/com/blogspot/httpgplaysupport/creditcardgeneratorpro/MainFragment.java
@@ -0,0 +1,243 @@
+package com.blogspot.httpgplaysupport.creditcardgeneratorpro;
+
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.SecureRandom;
+import java.util.Calendar;
+import java.util.Random;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment implements View.OnClickListener {
+
+    int cardIIN;
+    int cardUNLength;
+
+    static String finalCardNumString;
+
+    static final String CVV = "0123456789";
+    static SecureRandom cvvRnd = new SecureRandom();
+
+    Spinner cardSelect;
+    ArrayAdapter<CharSequence> adapter;
+
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myFragmentView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button switchFragButton = myFragmentView.findViewById(R.id.checkFragmentButton);
+        switchFragButton.setOnClickListener(this);
+
+        ImageButton genButton = myFragmentView.findViewById(R.id.button);
+        genButton.setOnClickListener(this);
+
+        //sets up adapter to read array list used to populate spinner widget.
+        cardSelect = (Spinner) myFragmentView.findViewById(R.id.cardSelect);
+        adapter = ArrayAdapter.createFromResource(getActivity(), R.array.card_arrays, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cardSelect.setAdapter(adapter);
+
+
+        //can use getItemIdAtPosition or getItemAtPosition to identify which item in array is selected
+        //
+        cardSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), parent.getItemAtPosition(position) + " selected", Toast.LENGTH_SHORT).show();
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+
+        return myFragmentView;
+    }
+
+
+    @Override
+    public void onClick(View v){
+
+        switch (v.getId()) {
+            case R.id.checkFragmentButton:
+                Log.d("Credit Card Number: ", "Value: ");
+                final FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.mainLayout, new CheckFragment());
+                ft.addToBackStack(null);
+                ft.commit();
+                break;
+
+        }
+        switch (v.getId()){
+            case R.id.button:
+
+                TextView displayView = getView().findViewById(R.id.cardNumberView);
+                TextView cvvViewText = getView().findViewById(R.id.cvvView);
+                TextView yearView = getView().findViewById(R.id.yearView);
+                // TextView monthView = (TextView) findViewById(R.id.monthView);
+
+                Random yearRnd = new Random();
+                int number = yearRnd.nextInt(16) + Calendar.getInstance().get(Calendar.YEAR);;
+                String myString = String.valueOf(number);
+
+                Random monthRnd = new Random();
+                int monthNumber = yearRnd.nextInt(12) + 1;
+                String monthString = String.valueOf(monthNumber);
+
+                cardLength();
+
+                String cardPreCheckNum = String.valueOf(cardIIN) + String.valueOf(cvvString(cardUNLength));
+
+                calculateCheckDigit(cardPreCheckNum);
+
+                String cardNumberString = cardPreCheckNum + calculateCheckDigit(cardPreCheckNum);
+                finalCardNumString = cardNumberString;
+
+
+                displayView.setText(finalCardNumString);
+
+                Log.d("Credit Card Number: ", "Value: " + cardNumberString);
+
+
+                //displayView.setText(cvvString(4).toString() + "-" + cvvString(4).toString()+ "-" + cvvString(4).toString()+ "-" + cvvString(4).toString());
+                cvvViewText.setText(cvvString(3).toString());
+                yearView.setText(monthString + "/" + myString);
+                //monthView.setText(monthString);
+
+
+                break;
+
+        }
+    }
+
+    String cvvString(int len) {
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+            sb.append(CVV.charAt(cvvRnd.nextInt(CVV.length())));
+        return sb.toString();
+
+    }
+
+    //Uses the itemID from the arrayList and then randomly sets the prefix possibilities for each card company. Also sets the User idetification number length. (16-cardIIN)
+    public void cardLength() {
+        if (cardSelect.getSelectedItemId() == 0) {
+            cardIIN = 4;
+            cardUNLength = 14;
+        } else if (cardSelect.getSelectedItemId() == 1) {
+            cardIIN = new Random().nextInt(56 - 51) + 51;
+            cardUNLength = 13;
+        } else if (cardSelect.getSelectedItemId() == 2) {
+            int twoValue = new Random().nextInt(3 - 1) + 1;
+            if (twoValue == 1) {
+                cardIIN = 34;
+            } else {
+                cardIIN = 37;
+            }
+            cardUNLength = 13;
+        } else if (cardSelect.getSelectedItemId() == 3) {
+            int threeValue = new Random().nextInt(4 - 1) + 1;
+            if (threeValue == 1) {
+                cardIIN = 6011;
+                cardUNLength = 11;
+            } else if (threeValue == 2) {
+                cardIIN = 64;
+                cardUNLength = 13;
+            } else if (threeValue == 3) {
+                cardIIN = 65;
+                cardUNLength = 13;
+            }
+        } else if (cardSelect.getSelectedItemId() == 4) {
+            int threeValue = new Random().nextInt(4 - 1) + 1;
+            if (threeValue == 1) {
+                cardIIN = new Random().nextInt(306 - 300) + 300;
+                cardUNLength = 12;
+            } else if (threeValue == 2) {
+                cardIIN = 3095;
+                cardUNLength = 11;
+            } else if (threeValue == 3) {
+                cardIIN = new Random().nextInt(40 - 38) + 38;
+                cardUNLength = 13;
+            }
+        } else if (cardSelect.getSelectedItemId() == 5) {
+            cardIIN = 62;
+            cardUNLength = 13;
+        } else if (cardSelect.getSelectedItemId() == 6) {
+            int twoValue = new Random().nextInt(3 - 1) + 1;
+            if (twoValue == 1) {
+                cardIIN = 60;
+                cardUNLength = 13;
+            } else {
+                cardIIN = 6521;
+                cardUNLength = 11;
+            }
+        }
+        else if (cardSelect.getSelectedItemId() == 7) {
+            cardIIN = new Random().nextInt(3590 - 3528) + 3528;
+            cardUNLength = 11;
+        }
+    }
+
+    public static String calculateCheckDigit(String card) {
+        if (card == null)
+            return null;
+        String digit;
+        /* convert to array of int for simplicity */
+        int[] digits = new int[card.length()];
+        for (int i = 0; i < card.length(); i++) {
+            digits[i] = Character.getNumericValue(card.charAt(i));
+        }
+
+        /* double every other starting from right - jumping from 2 in 2 */
+        for (int i = digits.length - 1; i >= 0; i -= 2)    {
+            digits[i] += digits[i];
+
+            /* taking the sum of digits grater than 10 - simple trick by substract 9 */
+            if (digits[i] >= 10) {
+                digits[i] = digits[i] - 9;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < digits.length; i++) {
+            sum += digits[i];
+        }
+        /* multiply by 9 step */
+        sum = sum * 9;
+
+        /* convert to string to be easier to take the last digit */
+        digit = sum + "";
+        return digit.substring(digit.length() - 1);
+    }
+
+
+}
